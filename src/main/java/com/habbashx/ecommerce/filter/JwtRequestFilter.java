754d013215a466d57c750d4b3d86f3f6b17ebbbb
HabbashX <abdalahalhabbash@gmail.com>
2026-03-08 14:55:38 +0200

    fix: ensure filter chain continues after authentication in JwtRequestFilter

--- src/main/java/com/habbashx/ecommerce/filter/JwtRequestFilter.java
+++ src/main/java/com/habbashx/ecommerce/filter/JwtRequestFilter.java
@@ -57,9 +57,11 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
                 usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                filterChain.doFilter(request,response);
             }
 
         }
+
 
     }
 }
